f90c4d300d435b97a1086dfbafd31e04174eb087
Ahmed Herhesh <ahmedherhesh3@gmail.com>
2025-10-15 22:51:25 +0300

    Request validation of stock and comment

--- api/Dtos/Comment/CommentRequestDto.cs
+++ api/Dtos/Comment/CommentRequestDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,10 @@ namespace api.Dtos.Comment
 {
     public class CommentRequestDto
     {
+        [Required, MinLength(5, ErrorMessage = "The title field must be at least 5 characters"), MaxLength(255)]
         public string Title { get; set; } = string.Empty;
+
+        [Required, MinLength(5, ErrorMessage = "The content field must be at least 5 characters"), MaxLength(255)]
         public string Content { get; set; } = string.Empty;
     }
 }
